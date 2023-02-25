e75831bce0b73167b6b081355394d4c39a39084c
Jacob Adams <jacob.adams121202@gmail.com>
2023-02-25 12:56:07 -0800

    course_grade_folder

---
+++ ch2/labs/2_12_course_grade/main.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+int main() {
+
+   /* TODO: Declare any necessary variables here. */
+      
+      
+   /* TODO: Read a file name from the user and read the tsv file here. */
+   
+   
+   /* TODO: Compute student grades and exam averages, then output results to a text file here. */
+
+   return 0;
+}
