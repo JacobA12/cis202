a20e25f23b88fb99c3b4d9ec19bcfc215b466768
Jacob Adams <jacob.adams121202@gmail.com>
2023-02-26 12:34:49 -0800

    added control structure and logic

--- ch2/labs/2_13_parsing_food_data/main.cpp
+++ ch2/labs/2_13_parsing_food_data/main.cpp
@@ -18,8 +18,23 @@ int main()
 
    while (getline(inFS, currentLine))
    {
-   
-   }
+      //getting tab positions
+      unsigned int pos1 = currentLine.find('\t');
+      unsigned int pos2 = currentLine.find('\t', pos1 + 1);
+      unsigned int pos3 = currentLine.find('\t', pos2 + 1);
 
+      //parsing food data
+      string currentCategory, currentName, currentDescription, currentAvailability;
+      currentCategory = currentLine.substr(0, pos1);
+      currentName = currentLine.substr(pos1 + 1, pos2 - 1);
+      currentDescription = currentLine.substr(pos2 + 1, pos3 - 1);
+      currentAvailability = currentLine.substr(pos3 + 1);
+
+      if(currentAvailability == "Available"){
+         cout << currentName << " (" << currentCategory << ") -- " << currentDescription << endl;
+      }
+
+   }
+   inFS.close();
    return 0;
 }
