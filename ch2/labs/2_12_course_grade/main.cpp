0e4fa597dfb61de6c71ab0706f9ee648a43b8c04
Jacob Adams <jacob.adams121202@gmail.com>
2023-02-26 10:34:34 -0800

    Added grade function, variables, file opening

--- ch2/labs/2_12_course_grade/main.cpp
+++ ch2/labs/2_12_course_grade/main.cpp
@@ -5,22 +5,53 @@
 #include <vector>
 using namespace std;
 
-int main() {
+char letterGradeConversion(double examScore)
+{
+   if (examScore >= 90)
+   {
+      return 'A';
+   }
+   else if (examScore >= 80)
+   {
+      return 'B';
+   }
+   else if (examScore >= 70)
+   {
+      return 'C';
+   }
+   else if (score >= 60)
+   {
+      return 'D';
+   }
+   else
+   {
+      return 'F';
+   }
+}
+
+int main()
+{
 
    /* TODO: Declare any necessary variables here. */
    ifstream inFS;
    string fileName;
 
-
    double averageScore;
+   int midterm1;
+   int midterm2;
+   int finalExam;
    string lastName;
    string firstName;
-   vector<int> examScores;
    char letterGrade;
-      
+
    /* TODO: Read a file name from the user and read the tsv file here. */
-   
-   
+   cin >> fileName;
+   inFS.open(fileName);
+   if (!inFS.is_open())
+   {
+      return 1;
+   }
+
    /* TODO: Compute student grades and exam averages, then output results to a text file here. */
 
    return 0;
