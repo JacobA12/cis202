5c7e4703ce185f2450a9d75906457332c2a46bc4
Jacob Adams <jacob.adams121202@gmail.com>
2023-02-27 23:28:01 -0800

    final commit

--- ch2/labs/2_13_parsing_food_data/main.cpp
+++ ch2/labs/2_13_parsing_food_data/main.cpp
@@ -18,22 +18,36 @@ int main()
 
    while (getline(inFS, currentLine))
    {
-      //getting tab positions
+
+      // getting tab positions
       unsigned int pos1 = currentLine.find('\t');
       unsigned int pos2 = currentLine.find('\t', pos1 + 1);
       unsigned int pos3 = currentLine.find('\t', pos2 + 1);
 
-      //parsing food data
-      string currentCategory, currentName, currentDescription, currentAvailability;
-      currentCategory = currentLine.substr(0, pos1);
-      currentName = currentLine.substr(pos1 + 1, pos2 - 1);
-      currentDescription = currentLine.substr(pos2 + 1, pos3 - 1);
-      currentAvailability = currentLine.substr(pos3 + 1);
-
-      if(currentAvailability == "Available"){
-         cout << currentName << " (" << currentCategory << ") -- " << currentDescription << endl;
+      if (pos1 == string::npos)
+      {
+         continue;
+      }
+      if (pos2 == string::npos)
+      {
+         continue;
+      }
+      if (pos3 == string::npos)
+      {
+         continue;
       }
 
+      // parsing food data
+      string currentCategory, currentName, currentDescription, currentAvailability;
+      currentCategory = currentLine.substr(0, pos1);
+      currentName = currentLine.substr(pos1 + 1, pos2 - pos1 - 1);
+      currentDescription = currentLine.substr(pos2 + 1, pos3 - pos2 - 1);
+      currentAvailability = currentLine.substr(pos3 + 1);
+
+      if (currentAvailability == "Available")
+      {
+         cout << currentName << " (" << currentCategory << ") -- " << currentDescription << endl;
+      }
    }
    inFS.close();
    return 0;
