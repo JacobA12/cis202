07d94c23a909eb346d15435972bf17e0010253b4
Jacob Adams <jacob.adams121202@gmail.com>
2023-02-28 13:38:50 -0800

    inventory + playlist lab

---
+++ cis202/ch3/3_16_playlist/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "SongNode.h"
+
+// TODO: Write PrintPlaylist() function
+
+int main() {
+	SongNode* headNode;
+	SongNode* currNode;
+	SongNode* lastNode;
+
+	string songTitle;
+	string songLength;
+	string songArtist;
+
+	// Front of nodes list                                                                         
+	headNode = new SongNode();
+	lastNode = headNode;
+
+	// Read user input until -1  entered
+	getline(cin, songTitle);
+	while (songTitle != "-1") {
+		getline(cin, songLength);
+		getline(cin, songArtist);
+
+		currNode = new SongNode(songTitle, songLength, songArtist);
+		lastNode->InsertAfter(currNode);
+		lastNode = currNode;
+
+		getline(cin, songTitle);
+	}
+
+	// Print linked list
+	cout << "LIST OF SONGS" << endl;
+	cout << "-------------" << endl;
+	PrintPlaylist(headNode);
+	
+	return 0;
+}
