2df211515edce04ab0a773f14fb4bd082338eda7
Jacob Adams <jacob.adams121202@gmail.com>
2023-03-02 15:40:51 -0800

    final commit runner

--- cis202/ch3/3_14_mileage_tracker_for_a_runner/main.cpp
+++ cis202/ch3/3_14_mileage_tracker_for_a_runner/main.cpp
@@ -18,13 +18,28 @@ int main () {
    lastNode = headNode;
 
    // TODO: Read in the number of nodes
-   
+   int numNodes;
+   cin >> numNodes;
    // TODO: For the read in number of nodes, read
    //       in data and insert into the linked list
+   for (int i = 0; i < numNodes; i++)
+   {
+      cin >> miles;
+      cin >> date;
 
+      MileageTrackerNode *newNode = new MileageTrackerNode(miles, date);
+      lastNode->InsertAfter(newNode);
+      lastNode = newNode;
+   }
+   
    // TODO: Call the PrintNodeData() method
    //       to print the entire linked list
-
+   currNode = headNode ->GetNext();
+   while(currNode)
+   {
+      currNode->PrintNodeData();
+      currNode = currNode->GetNext();
+   }
    // MileageTrackerNode Destructor deletes all
    //       following nodes
    delete headNode;
