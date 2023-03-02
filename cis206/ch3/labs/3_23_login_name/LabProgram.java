3545c6ebba9c090b81ab53ec3268a89ee755c794
Jacob Adams <jacob.adams121202@gmail.com>
2023-03-02 08:49:31 -0800

    login_name final commit

--- cis206/ch3/labs/3_23_login_name/LabProgram.java
+++ cis206/ch3/labs/3_23_login_name/LabProgram.java
@@ -4,7 +4,33 @@ public class LabProgram {
 
 	public static void main(String[] args) {
 
-		/* Type your code here. */
+		String firstName;
+		String lastName;
+		int userNum;
+
+		Scanner sc = new Scanner(System.in);
+
+		firstName = sc.next();
+		lastName = sc.next();
+		userNum = sc.nextInt();
+
+		sc.close();
+
+		String loginName = "";
+
+		if (lastName.length() < 5) {
+			loginName = loginName.concat(lastName);
+		} else {
+			loginName = loginName.concat(lastName.substring(0, 5));
+		}
+
+		loginName = loginName + firstName.charAt(0);
+
+		int lastTwoNum = userNum % 100;
+
+		loginName = loginName.concat(String.valueOf(lastTwoNum));
+
+		System.out.println("Your login name: " + loginName);
 
 	}
 }
