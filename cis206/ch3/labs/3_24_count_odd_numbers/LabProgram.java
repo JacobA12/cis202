293577517ec3403389b7b75eecf7446d180ff88f
Jacob Adams <jacob.adams121202@gmail.com>
2023-03-02 08:57:43 -0800

    odd_num finalcommit

--- cis206/ch3/labs/3_24_count_odd_numbers/LabProgram.java
+++ cis206/ch3/labs/3_24_count_odd_numbers/LabProgram.java
@@ -4,6 +4,15 @@ public class LabProgram {
    public static void main(String[] args) {
       Scanner scnr = new Scanner(System.in);
 
-      /* Type your code here. */
+      int count = 0;
+      for (int i = 0; i < 4; i++) {
+         int userNum = scnr.nextInt();
+         if (userNum % 2 != 0){
+             count++;
+         }
+         
+      }
+      scnr.close();
+      System.out.println(count);
    }
 }
