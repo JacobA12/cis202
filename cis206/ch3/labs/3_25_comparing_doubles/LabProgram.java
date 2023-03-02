6a81ee92b6171676f3db5476396abba7a0710818
Jacob Adams <jacob.adams121202@gmail.com>
2023-03-02 09:12:17 -0800

    comparing_doubles final commit

--- cis206/ch3/labs/3_25_comparing_doubles/LabProgram.java
+++ cis206/ch3/labs/3_25_comparing_doubles/LabProgram.java
@@ -4,6 +4,19 @@ public class LabProgram {
    public static void main(String[] args) {
       Scanner scnr = new Scanner(System.in);
 
-      /* Type your code here. */
+      double num1 = scnr.nextDouble();
+      double num2 = scnr.nextDouble();
+      double epsilon = scnr.nextDouble();
+
+      scnr.close();
+
+      if (Math.abs(num1 - num2) <= 0.001) {
+         System.out.println("equal");
+      } else if (Math.abs(num1 - num2) <= epsilon) {
+         System.out.println("close enough");
+      } else {
+         System.out.println("not close");
+      }
+      
    }
 }
