4e2be01c85e4d663de9a9af5f4de801dd3dd7c74
Jacob Adams <jacob.adams121202@gmail.com>
2023-03-02 09:27:54 -0800

    need to add checks for invalid day

--- cis206/ch3/labs/3_26_seasons/LabProgram.java
+++ cis206/ch3/labs/3_26_seasons/LabProgram.java
@@ -1,11 +1,67 @@
-import java.util.Scanner; 
+import java.util.Scanner;
 
 public class LabProgram {
    public static void main(String[] args) {
-      Scanner scnr = new Scanner(System.in); 
+      Scanner scnr = new Scanner(System.in);
       String inputMonth;
       int inputDay;
-      
-      /* Type your code here. */
+
+      inputMonth = scnr.next();
+      inputDay = scnr.nextInt();
+
+      scnr.close();
+      inputMonth = inputMonth.toLowerCase();
+      String season = "";
+      switch (inputMonth) {
+         case "january":
+         case "february":
+            season = "Winter";
+            break;
+         case "march":
+            if (inputDay <= 19) {
+               season = "Winter";
+            } else {
+               season = "Spring";
+            }
+            break;
+         case "april":
+         case "may":
+            season = "Spring";
+            break;
+         case "june":
+            if (inputDay <= 20) {
+               season = "Spring";
+            } else {
+               season = "Summer";
+            }
+            break;
+         case "july":
+         case "august":
+            season = "Summer";
+            break;
+         case "september":
+            if (inputDay <= 21) {
+               season = "Summer";
+            } else {
+               season = "Autumn";
+            }
+            break;
+         case "october":
+         case "november":
+            season = "Autumn";
+            break;
+         case "december":
+            if (inputDay <= 20) {
+               season = "Autumn";
+            } else {
+               season = "Winter";
+            }
+            break;
+         default:
+            season = "Invalid";
+            break;
+      }
+      System.out.println(season);
+
    }
 }
