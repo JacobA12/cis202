19483e2474dd27c396d2eb7a3b9f84b045690390
Jacob Adams <jacob.adams121202@gmail.com>
2023-03-07 12:35:07 -0800

    count characters lab

--- cis206/ch4/4_14_count_characters/LabProgram.java
+++ cis206/ch4/4_14_count_characters/LabProgram.java
@@ -2,6 +2,22 @@ import java.util.Scanner;
 
 public class LabProgram {
    public static void main(String[] args) {
-      /* Type your code here. */
+      Scanner scnr = new Scanner(System.in);
+
+      char userInput = scnr.next().charAt(0);
+      String userInputStr = scnr.nextLine();
+
+      scnr.close();
+      int count = 0;
+      for (int i = 0; i < userInputStr.length(); i++) {
+         if (userInputStr.charAt(i) == userInput) {
+            count++;
+         }
+      }
+      if (count != 1) {
+         System.out.println(count + " " + userInput + "'s");
+      } else {
+         System.out.println(count + " " + userInput);
+      }
    }
 }
