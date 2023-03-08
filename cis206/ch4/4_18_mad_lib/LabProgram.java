c74ab0b6f43e2946020bb800dc4e185e74406e39
Jacob Adams <jacob.adams121202@gmail.com>
2023-03-07 16:36:49 -0800

    final commit

--- cis206/ch4/4_18_mad_lib/LabProgram.java
+++ cis206/ch4/4_18_mad_lib/LabProgram.java
@@ -2,6 +2,15 @@ import java.util.Scanner;
 
 public class LabProgram {
    public static void main(String[] args) {
-      /* Type your code here. */
-   }
+      Scanner scnr = new Scanner(System.in);
+      String userInput = scnr.next();
+      int userNum = scnr.nextInt();
+      
+      while (!userInput.equalsIgnoreCase("done")){
+         System.out.println("Eating " + userNum + " " + userInput + " a day keeps you happy and healthy.");
+         userInput = scnr.next();
+         userNum = scnr.nextInt();
+      }
+
+    }
 }
