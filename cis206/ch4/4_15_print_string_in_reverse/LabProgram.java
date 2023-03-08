cf967a8bacb5dcdc209190f13580d29d2313bb39
Jacob Adams <jacob.adams121202@gmail.com>
2023-03-07 16:03:39 -0800

    4_15 final commit

--- cis206/ch4/4_15_print_string_in_reverse/LabProgram.java
+++ cis206/ch4/4_15_print_string_in_reverse/LabProgram.java
@@ -2,6 +2,18 @@ import java.util.Scanner;
 
 public class LabProgram {
    public static void main(String[] args) {
-      /* Type your code here. */
+      Scanner scnr = new Scanner(System.in);
+
+      String userString = scnr.nextLine();
+      String reverseString = "";
+      while (!userString.equalsIgnoreCase("done") && !userString.equalsIgnoreCase("d")) {
+         for (int i = userString.length() - 1; i >= 0; i--) {
+            reverseString += userString.charAt(i);
+         }
+         System.out.println(reverseString);
+         userString = scnr.nextLine();
+         reverseString = "";
+      }
+      scnr.close();
    }
 }
