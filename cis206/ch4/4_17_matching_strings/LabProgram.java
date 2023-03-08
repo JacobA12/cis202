50267fd7f16d162c261fe57a163cdf8bdb0b5d70
Jacob Adams <jacob.adams121202@gmail.com>
2023-03-07 16:20:11 -0800

    4_17 final commit

--- cis206/ch4/4_17_matching_strings/LabProgram.java
+++ cis206/ch4/4_17_matching_strings/LabProgram.java
@@ -4,6 +4,30 @@ public class LabProgram {
    public static void main(String[] args) {
       Scanner scnr = new Scanner(System.in);
 
-      /* Type your code here. */
+      String inputOne = scnr.next();
+      String inputTwo = scnr.next();
+
+      scnr.close();
+      String smallerString;
+      if (inputOne.length() < inputTwo.length()) {
+         smallerString = inputOne;
+      }else{
+         smallerString = inputTwo;
+      }
+
+      int count = 0;
+
+      for(int i = 0; i < smallerString.length(); i++){
+         if(inputOne.charAt(i) == inputTwo.charAt(i)){
+            count++;
+         }
+      }
+      if(count > 1){
+         System.out.println(count + " characters match");
+      }else if (count == 1){
+         System.out.println(count + " character matches");
+      }else{
+         System.out.println(count + " characters match");
+      }
    }
 }
