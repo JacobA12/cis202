affaf897d7551fe2ee3873b80226e3b19df9db22
Jacob Adams <jacob.adams121202@gmail.com>
2023-03-07 16:28:48 -0800

    4_16 final commit

--- cis206/ch4/4_16_months_until_payoff/LabProgram.java
+++ cis206/ch4/4_16_months_until_payoff/LabProgram.java
@@ -3,7 +3,23 @@ import java.util.Scanner;
 public class LabProgram {
    public static void main(String[] args) {
       Scanner scnr = new Scanner(System.in);
+      double loanAmount = scnr.nextDouble();
+      double paymentAmount = scnr.nextDouble();
+      double interestRate = scnr.nextDouble();
+      int count = 0;
 
-      /* Type your code here. */
+      while (loanAmount > 0) {
+         double interestAmount = loanAmount * interestRate;
+
+         loanAmount += interestAmount;
+         loanAmount -= paymentAmount;
+         count++;
+      }
+      if (count == 1) {
+         System.out.println(count + " payment");
+      } else {
+         System.out.println(count + " payments");
+      }
+
    }
 }
