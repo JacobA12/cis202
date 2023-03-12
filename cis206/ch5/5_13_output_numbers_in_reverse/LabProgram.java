625e1f36407b5ede9195b5ee3ff0400eb24ce101
Jacob Adams <jacob.adams121202@gmail.com>
2023-03-12 13:41:58 -0700

    first mistakes

--- cis206/ch5/5_13_output_numbers_in_reverse/LabProgram.java
+++ cis206/ch5/5_13_output_numbers_in_reverse/LabProgram.java
@@ -3,12 +3,24 @@ import java.util.Scanner;
 public class LabProgram {
    public static void main(String[] args) {
       Scanner scnr = new Scanner(System.in);
-      int[] userList = new int[20];   // List of numElement integers specified by the user
-      int numElements;                // Number of integers in user's list
+      int numElements; // Number of integers in user's list
+      numElements = scnr.nextInt(); // Input begins with number of integers that follow
+     
+      int[] userList = new int[numElements]; // List of numElement integers specified by the user
       // Add more variables as needed
+      int[] reverseList = new int[numElements];
+      int userNum;
 
-      numElements = scnr.nextInt();   // Input begins with number of integers that follow
-      
+
       /* Type your code here. */
+      for (int i = numElements; i >= 0; i--) {
+         for (int j = 0; j < numElements; j++) {
+            userNum = scnr.nextInt();
+            reverseList[j] = userList[i];
+         }
+      }
+      for (int i = 0; i < reverseList.length; i++) {
+         System.out.print(reverseList[i] + " ");
+      }
    }
 }
