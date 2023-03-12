83cb7da3d7df0603fd53fc3952836f67b3500c93
Jacob Adams <jacob.adams121202@gmail.com>
2023-03-12 13:45:35 -0700

    no nested for loop, final commit

--- cis206/ch5/5_13_output_numbers_in_reverse/LabProgram.java
+++ cis206/ch5/5_13_output_numbers_in_reverse/LabProgram.java
@@ -3,26 +3,26 @@ import java.util.Scanner;
 public class LabProgram {
    public static void main(String[] args) {
       Scanner scnr = new Scanner(System.in);
-      int numElements; // Number of integers in user's list
-      numElements = scnr.nextInt(); // Input begins with number of integers that follow
+      int numElements; 
+      numElements = scnr.nextInt(); 
      
-      int[] userList = new int[numElements]; // List of numElement integers specified by the user
-      // Add more variables as needed
-      int[] reverseList = new int[numElements];
+      int[] userList = new int[numElements]; 
       
+      int[] reverseList = new int[numElements];
 
-
-      /* Type your code here. */
-      for(int i = 0; i < numElements; i++){
+      
+      for (int i = 0; i < numElements; i++) {
          userList[i] = scnr.nextInt();
       }
-      for (int i = numElements - 1; i >= 0; i--) {
-         for (int j = 0; j < numElements; j++) {
-            reverseList[j] = userList[i];
-         }
+
+     
+      for (int i = 0; i < numElements; i++) {
+         reverseList[i] = userList[numElements - 1 - i];
       }
+
+      
       for (int i = 0; i < reverseList.length; i++) {
-         System.out.print(reverseList[i] + " ");
+         System.out.print(reverseList[i] + ",");
       }
    }
 }
