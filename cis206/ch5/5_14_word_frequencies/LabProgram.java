ecd839cfcf847ff57616e6ae713cd6bf51e955d1
Jacob Adams <jacob.adams121202@gmail.com>
2023-03-14 12:44:35 -0700

    skeleton

--- cis206/ch5/5_14_word_frequencies/LabProgram.java
+++ cis206/ch5/5_14_word_frequencies/LabProgram.java
@@ -1,7 +1,21 @@
-import java.util.Scanner; 
+import java.util.Scanner;
 
 public class LabProgram {
    public static void main(String[] args) {
-      /* Type your code here. */
+      Scanner sc = new Scanner(System.in);
+
+      int wordCount = sc.nextInt();
+      int freqCount = 0;
+
+      String userWords[] = new String[wordCount];
+
+      int frequencies[] = new int[wordCount];
+      for (int i = 0; i < wordCount; i++) {
+         userWords[i] = sc.next();
+      }
+
+      for (int i = 0; i < wordCount; i++) {
+         
+      }
    }
 }
