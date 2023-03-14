3c4067e59615e9e32edb3584d9e93c63432db86b
Jacob Adams <jacob.adams121202@gmail.com>
2023-03-14 14:17:55 -0700

    final commit

--- cis206/ch5/5_17_sum_of_products/LabProgram.java
+++ cis206/ch5/5_17_sum_of_products/LabProgram.java
@@ -1,14 +1,26 @@
-import java.util.Scanner; 
+import java.util.Scanner;
 
 public class LabProgram {
    public static void main(String[] args) {
       Scanner scnr = new Scanner(System.in);
       int size;
-      
+
       size = scnr.nextInt();
-      int[] listA = new int[size];   // List A
-      int[] listB = new int[size];   // List B
+      int productVal = 0;
+      int[] listA = new int[size]; // List A
+      int[] listB = new int[size]; // List B
 
       /* Type your code here. */
+      for (int i = 0; i < size; i++) {
+         listA[i] = scnr.nextInt();
+      }
+      for (int j = 0; j < size; j++) {
+         listB[j] = scnr.nextInt();
+      }
+      scnr.close();
+      for (int i = 0; i < size; i++) {
+         productVal += listA[i] * listB[i];
+      }
+      System.out.println(productVal);
    }
 }
