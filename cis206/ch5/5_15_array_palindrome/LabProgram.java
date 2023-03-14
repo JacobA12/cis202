c66fda24202722740f9a8455f37a4f5960e38673
Jacob Adams <jacob.adams121202@gmail.com>
2023-03-14 13:55:36 -0700

    palindrome final commit

--- cis206/ch5/5_15_array_palindrome/LabProgram.java
+++ cis206/ch5/5_15_array_palindrome/LabProgram.java
@@ -1,10 +1,33 @@
-import java.util.Scanner; 
+import java.util.Scanner;
 
 public class LabProgram {
    public static void main(String[] args) {
       Scanner scnr = new Scanner(System.in);
-      int[] userValues = new int[20];   // List of integers from input
+      int arrLen = scnr.nextInt();
+      int[] userValues = new int[arrLen]; // List of integers from input
+      int[] backValues = new int[arrLen];
 
-      /* Type your code here. */
+      for (int i = 0; i < backValues.length; i++) {
+         userValues[i] = scnr.nextInt();
+      }
+
+      scnr.close();
+
+      for (int i = 0; i < backValues.length; i++) {
+         backValues[i] = userValues[arrLen - 1 - i];
+      }
+
+      boolean isPalindrome = true;
+      for (int i = 0; i < arrLen; i++) {
+         if (userValues[i] != backValues[i]) {
+            isPalindrome = false;
+            break;
+         }
+      }
+      if (isPalindrome) {
+         System.out.println("yes");
+      } else {
+         System.out.println("no");
+      }
    }
 }
