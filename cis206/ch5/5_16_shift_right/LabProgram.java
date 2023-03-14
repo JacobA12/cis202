8bf8cee49368bb412fcc09e020d1763893d7edf2
Jacob Adams <jacob.adams121202@gmail.com>
2023-03-14 14:29:36 -0700

    final commit

--- cis206/ch5/5_16_shift_right/LabProgram.java
+++ cis206/ch5/5_16_shift_right/LabProgram.java
@@ -1,10 +1,29 @@
-import java.util.Scanner; 
+import java.util.Scanner;
 
 public class LabProgram {
    public static void main(String[] args) {
       Scanner scnr = new Scanner(System.in);
-      int[] userValues = new int[20];   // List of integers from input
+      int numVal = scnr.nextInt();
+      int[] userValues = new int[numVal]; // List of integers from input
 
-      /* Type your code here. */
+      for (int i = 0; i < numVal; i++) {
+         userValues[i] = scnr.nextInt();
+      }
+
+      scnr.close();
+
+      int lastVal = userValues[numVal - 1];
+
+      for (int i = numVal - 1; i > 0; i--) {
+
+         userValues[i] = userValues[i - 1];
+      }
+
+      userValues[0] = lastVal;
+
+      for (int i = 0; i < numVal; i++) {
+         System.out.print(userValues[i] + " ");
+      }
+      System.out.println();
    }
 }
