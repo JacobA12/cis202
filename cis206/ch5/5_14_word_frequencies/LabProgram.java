d7ac1a759b7167bf2180b3a0b8baced2fb6acea6
Jacob Adams <jacob.adams121202@gmail.com>
2023-03-14 13:37:02 -0700

    Still having issues

--- cis206/ch5/5_14_word_frequencies/LabProgram.java
+++ cis206/ch5/5_14_word_frequencies/LabProgram.java
@@ -5,17 +5,25 @@ public class LabProgram {
       Scanner sc = new Scanner(System.in);
 
       int wordCount = sc.nextInt();
-      int freqCount = 0;
 
       String userWords[] = new String[wordCount];
 
       int frequencies[] = new int[wordCount];
-      for (int i = 0; i < wordCount; i++) {
-         userWords[i] = sc.next();
-      }
 
       for (int i = 0; i < wordCount; i++) {
-         
+         String word = sc.next();
+         userWords[i] = word;
+         frequencies[i] = 1;
+         for (int j = 0; j < i; j++) {
+            if (userWords[j].equals(word)) {
+               frequencies[j]++;
+               break;
+            }
+
+         }
+      }
+      for (int i = 0; i < frequencies.length; i++) {
+         System.out.println(userWords[i] + " - " + frequencies[i]);
       }
    }
 }
